dcd8ebc9fadc1b29fde779c1b1610778b289161b
Kavish Rizvi <rizvikavish205.com>
2022-04-26 13:17:44 +0530

    Initial Commit

---
+++ src/main/java/com/hpamp/eventnotificationms/EventNotificationMsApplication.java
@@ -0,0 +1,13 @@
+package com.hpamp.eventnotificationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotificationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventNotificationMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hpamp/eventnotificationms/EventNotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpamp.eventnotificationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificationMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
