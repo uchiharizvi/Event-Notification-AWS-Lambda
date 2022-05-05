00c4bc9c4334821153b2b8b5cb66d07ce12f03dd
Kavish Rizvi <rizvikavish205.com>
2022-05-05 17:21:12 +0530

    dependencies

--- src/main/java/com/hpamp/eventnotificationms/EventNotificationMsApplication.java
+++ src/main/java/com/hpamp/eventnotificationms/EventNotificationMsApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventNotificationMsApplication {
+public class EventNotificationMsApplication /*implements ApplicationContextInitializer<GenericApplicationContext>*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(EventNotificationMsApplication.class, args);
     }
 
+    /*@Override
+    public void initialize(GenericApplicationContext context) {
+context.registerBean("function", FunctionRe);
+    }*/
 }
---
+++ src/main/java/com/hpamp/eventnotificationms/handler/ServiceHandler.java
@@ -0,0 +1,12 @@
+package com.hpamp.eventnotificationms.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class ServiceHandler implements RequestHandler<String, Object> {
+    @Override
+    public Object handleRequest(String s, Context context) {
+        context.getLogger().log("Input : " + s);
+        return "Lambda Function is invoked...." + s;
+    }
+}
